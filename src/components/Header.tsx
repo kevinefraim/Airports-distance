2b78172b32d55d6a7c06c81e3939be7b08e8448a
kevinefraim <kevinefraim02@gmail.com>
2022-08-12 14:47:48 -0300

    setting header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <div className="bg-gray-200 flex h-20 justify-center items-center">
+      <Heading>Airports - distance</Heading>
+    </div>
+  );
+};
+
+export default Header;
