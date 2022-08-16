8b6199a473fae2a7d7be564253b651e77f5c82db
kevinefraim <kevinefraim02@gmail.com>
2022-08-16 16:07:54 -0300

    adding main tag

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,10 @@
-import { Box } from "@mui/material";
-
 import MainContainer from "./MainContainer";
 
 const Home = () => {
   return (
-    <Box className="h-[60%] flex items-center justify-center">
+    <main className="h-[60%] flex items-center justify-center">
       <MainContainer />
-    </Box>
+    </main>
   );
 };
 
