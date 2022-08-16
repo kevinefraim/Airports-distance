1e9636c957f2ceab710b3582d5cfa3c39098c2e8
kevinefraim <kevinefraim02@gmail.com>
2022-08-16 11:33:15 -0300

    adding loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import Header from "components/Header";
 import Home from "components/Home";
 import Loader from "components/Loader";
+import { useEffect, useState } from "react";
 
 import { useLoadScript } from "@react-google-maps/api";
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
     libraries: ["places"],
   });
-  if (!isLoaded) return <Loader />;
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
+
+  if (!isLoaded || loading) return <Loader />;
 
   return (
     <>
